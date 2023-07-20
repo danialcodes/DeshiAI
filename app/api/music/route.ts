85d89f80df7ad016e3398e98e68e737184397ee9
danialcodes <danialcodes@gmail.com>
2023-07-21 05:41:50 +0600

    added: all ai tool completed

---
+++ app/api/music/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+import Replicate from 'replicate';
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_KEY || "",
+});
+
+export const POST = async (
+    req: Request,
+) => {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { prompt } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        if (!replicate.auth) {
+            return new NextResponse("Replicate API key not configured", { status: 500 });
+        }
+        if (!prompt) {
+            return new NextResponse("Prompt is required", { status: 400 });
+        }
+        const response = await replicate.run(
+            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+            {
+                input: {
+                    prompt_a: prompt
+                }
+            }
+        );
+        return NextResponse.json(response);
+    }
+    catch (err) {
+        console.log("[Music error]", err);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
